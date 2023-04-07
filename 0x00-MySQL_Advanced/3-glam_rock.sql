ecf4dc2b894ce5e6d96358b884c487287cf44c4a
frank-gich <gichuhi5781@gmail.com>
2023-04-08 00:14:01 +0300

    my  push alx backend storage

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
--- Lists all bands with Glam as their main style, ranked by their longevity
+-- Lists all bands with Glam as their main style, ranked by their longevity --
 
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) as lifespan
 FROM metal_bands
 WHERE style like '%Glam rock%';
+ORDER BY lifespan DESC;
